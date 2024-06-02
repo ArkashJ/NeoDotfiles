c75ee0737d8a5e2bd4cdf6514aeb78977d6cba5a
Arkash Jain <86805856+ArkashJ@users.noreply.github.com>
2024-06-02 12:26:37 +0530

    split window keymaps, leader sv for vertical, leader sh for horizontal, leader se for equal size and leader sx for closing it

--- lua/arkash/core/keymaps.lua
+++ lua/arkash/core/keymaps.lua
@@ -11,3 +11,8 @@ keymap.set("n", "<leader>nh", ":nohl<CR>", {desc="Clear search highlights"})
 keymap.set("n", "<leader>+", "<C-a>", {desc = "Increment Number"}) -- increment
 keymap.set("n", "<leader>-", "<C-x>", {desc = "Decrement Number"}) -- decrement
 
+-- window management
+keymap.set("n", "<leader>sv", "<C-w>v", {desc="Split window vertically"})
+keymap.set("n", "<leader>sh", "<C-w>s", {desc="Split window horizontally"})
+keymap.set("n", "<leader>se", "<C-w>=", {desc="Make splits equal size"})
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", {desc="Close current split"})
