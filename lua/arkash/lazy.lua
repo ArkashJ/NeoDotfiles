65a0c73605e74966637360d664fcf23bc3082d2d
Arkash Jain <86805856+ArkashJ@users.noreply.github.com>
2024-06-02 23:33:43 +0530

    add mason

--- lua/arkash/lazy.lua
+++ lua/arkash/lazy.lua
@@ -10,4 +10,4 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup("arkash.plugins")
+require("lazy").setup({ {import="arkash.plugins"}, {import="arkash.plugins.lsp"} })
