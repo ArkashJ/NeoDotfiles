71f0ffbd892ddb0a573619211be669152fabf875
Arkash Jain <86805856+ArkashJ@users.noreply.github.com>
2024-06-02 13:16:50 +0530

    adding colorschemes and nvim tree

---
+++ lua/arkash/plugins/init.lua
@@ -0,0 +1,6 @@
+-- using this file to store plugins that do not require much config
+
+return {
+  "nvim-lua/plenary.nvim", -- lua functions that many plugins use
+  "christoomey/vim-tmux-navigator", -- tmux & split window nav
+}
