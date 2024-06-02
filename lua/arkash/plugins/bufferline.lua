4a6a1f421a74181a3566053ba7d617144eaaf78c
Arkash Jain <86805856+ArkashJ@users.noreply.github.com>
2024-06-02 21:46:01 +0530

    adding lualine

--- lua/arkash/plugins/bufferline.lua
+++ lua/arkash/plugins/bufferline.lua
@@ -13,18 +13,6 @@ return {
       left_trunc_marker = '',
       right_trunc_marker = '',
       diagnostics =  "nvim_lsp",
-      color_icons = true,
-      show_buffer_icons = true,
-      show_buffer_close_icons = true,
-      show_close_icon = true,
-      show_tab_indicators = true ,
-      show_duplicate_prefix = true, 
-      auto_toggle_bufferline = true ,
-      hover = {
-          enabled = true,
-          delay = 200,
-          reveal = {'close'}
-      },
     }
   }
 }
