48aa421ebf9e4cf4f4a8733860228b0b9fe6e71b
Arkash Jain <86805856+ArkashJ@users.noreply.github.com>
2024-06-02 21:50:44 +0530

    updating lualine

--- lua/arkash/plugins/lualine.lua
+++ lua/arkash/plugins/lualine.lua
@@ -3,6 +3,7 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
     local lualine = require("lualine")
+    local lazy_status = require("lazy.status")
     lualine.setup({
       options ={
       icons_enabled = true,
@@ -26,9 +27,18 @@ return {
           lualine_a = {'mode'},
           lualine_b = {'branch', 'diff', 'diagnostics'},
           lualine_c = {'filename'},
-          lualine_x = {'encoding', 'fileformat', 'filetype'},
-          lualine_y = {'progress'},
-          lualine_z = {'location'}
+          lualine_x = {
+          {
+            lazy_status.updates,
+            cond = lazy_status.has_updates,
+            color = { fg = "#ff9e64" },
+          },
+          { "encoding" },
+          { "fileformat" },
+          { "filetype" },
+        },          
+        lualine_y = {'progress'},
+        lualine_z = {'location'}
         },
         inactive_sections = {
           lualine_a = {},
