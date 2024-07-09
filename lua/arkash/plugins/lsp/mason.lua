ec108ee8a0f708a1faa055e530c550400f79e724
Arkash Jain <86805856+ArkashJ@users.noreply.github.com>
2024-07-09 11:02:27 -0400

    updating config

--- lua/arkash/plugins/lsp/mason.lua
+++ lua/arkash/plugins/lsp/mason.lua
@@ -47,7 +47,7 @@ return {
         "stylua", -- lua formatter
         "isort", -- python formatter
         "black", -- python formatter
-        "pylint",
+        "ruff",
         "eslint_d",
       },
     })
