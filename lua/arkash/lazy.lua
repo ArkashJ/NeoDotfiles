ac55261bb560ba45e6a415a7086de82b74547761
Arkash Jain <86805856+ArkashJ@users.noreply.github.com>
2024-09-04 11:36:42 -0400

    update lazy

--- lua/arkash/lazy.lua
+++ lua/arkash/lazy.lua
@@ -10,4 +10,26 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup({ {import="arkash.plugins"}, {import="arkash.plugins.lsp"} })
+require("lazy").setup({
+  { import = "arkash.plugins" },
+  { import = "arkash.plugins.lsp" },
+  { import = "lazyvim.plugins.extras.coding.copilot" },
+  { import = "lazyvim.plugins.extras.editor.telescope" },
+  { import = "lazyvim.plugins.extras.formatting.prettier" },
+  { import = "lazyvim.plugins.extras.lang.clangd" },
+  { import = "lazyvim.plugins.extras.lang.cmake" },
+  { import = "lazyvim.plugins.extras.lang.docker" },
+  { import = "lazyvim.plugins.extras.lang.git" },
+  { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.python" },
+  { import = "lazyvim.plugins.extras.lang.rust" },
+  { import = "lazyvim.plugins.extras.lang.sql" },
+  { import = "lazyvim.plugins.extras.lang.tailwind" },
+  { import = "lazyvim.plugins.extras.lang.toml" },
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.yaml" },
+  { import = "lazyvim.plugins.extras.linting.eslint" },
+  { import = "lazyvim.plugins.extras.lsp.none-ls" },
+  { import = "lazyvim.plugins.extras.vscode" }, -- for nvim on VSCode
+  -- import/override with your plugins
+})
